a8342028226ec668d6d2736f73b103e5e3ddfff2
Daniel Yang <danielyang@Mac-Mini.local>
2022-07-27 13:08:45 +1200

    Up to setp 5

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from "axios"
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //Declaring new state vairable which we are gonna call "pokemonName"
+  const [pokemonName, setPokemonName] = useState("")
+  const [pokemonInfo, setPokemonInfo] = useState<undefined | any>(undefined); //State vairable to sotre info from API
+
+  const POKEMON_BASE_URL = "https://pokeapi.co/api/v2"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Pokemon Search
+      </h1>
+
+      <div>
+        <label>Pokemon Name</label>
+        <br />
+        <input
+          type='text'
+          id='pokemon-name'
+          name='pokmon-name'
+          onChange={e => setPokemonName(e.target.value)}
+        />
+        <br />
+        <button onClick={search}>
+          Search
+        </button>
+      </div>
+
+      <p>
+        You have entered {pokemonName}
+      </p>
+      {pokemonInfo === undefined ?
+        (
+          //If true
+          <p>Pokemon not found</p>
+        )
+        :
+        (
+          //If false
+          <div id="pokemon-result">
+            <img src={pokemonInfo.sprites.other.dream_world.front_default} />
+          </div>
+        )}
     </div>
   );
+
+  function search() {
+    axios.get(POKEMON_BASE_URL + "/pokemon/" + pokemonName).then((res) => {
+      setPokemonInfo(res.data);
+    });
+  }
 }
 
 export default App;
